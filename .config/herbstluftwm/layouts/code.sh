766203a324ddde1c38197636665f17b09e03c336
Martin Munstermann <martin@munstermann.io>
2020-04-03 13:30:03 +0200

    we are getting close to a working layout / client re-creation

--- .config/herbstluftwm/layouts/code.sh
+++ .config/herbstluftwm/layouts/code.sh
@@ -5,20 +5,25 @@
 #
 # Example output:
 # ╾─┐ horizontal 65% selection=0 <------ Note!
-#   ├─┐ horizontal 45% selection=0 <------ Note!
+#   ├─┐ horizontal 45% selection=1 <------ Note!
 #   │ ├─┐ vertical 50% selection=0 <------ Note!
 #   │ │ ├─╼ vertical:
 #   │ │ └─╼ vertical:
 #   │ └─╼ vertical:
 #   └─╼ vertical:
 #
-# Example frame index: 000
+# Example frame index: 010 (Read 'selection' from top to bottom)
 
 spawn_with_rules() {
     (
-        # this rule also requires, that the client
-        # sets the _NET_WM_PID property
-        herbstclient rule once pid=$BASHPID maxage=10 "${RULES[@]}"
+        # doe we have 'clientinfo' to use as identification
+        if [[ -z "$CIENTINFO" ]]; then
+            # no: we bet _NET_WM_PID property is set
+            herbstclient rule once pid=$BASHPID maxage=10 "${RULES[@]}"
+        else
+            # yes: we use client info
+            herbstclient rule once "${CIENTINFO}" maxage=10 "${RULES[@]}"
+        fi
         exec "$@"
     ) &
 }
@@ -27,17 +32,21 @@ herbstclient load 2 "$(< ~/.config/herbstluftwm/layouts/code.layout)"
 herbstclient use_index 1
 
 # LEFT TOP
+CIENTINFO=""
 RULES=( tag=2 index=000 focus=off )
-spawn_with_rules urxvt -e nano lefttop.txt
+spawn_with_rules urxvt
 
 # LEFT BOTTOM
+CIENTINFO=""
 RULES=( tag=2 index=001 focus=off )
-spawn_with_rules urxvt -e nano leftbottom.txt
+spawn_with_rules urxvt
 
 # CENTER
+CIENTINFO="class=Code"
 RULES=( tag=2 index=011 focus=off )
 spawn_with_rules code
 
 # RIGHT
+CIENTINFO=""
 RULES=( tag=2 index=111 focus=off )
 spawn_with_rules firefox
